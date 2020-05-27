5b80c7d0e9f111c10253efd3c0405d0276e0c37f
looksocii <aopup1122@gmail.com>
2020-05-27 19:49:53 +0700

    by aop

---
+++ Code (Django Project)/static/home_js.js
@@ -0,0 +1,55 @@
+ var dropFileForm = document.getElementById("dropFileForm");
+  var fileLabelText = document.getElementById("fileLabelText");
+  var uploadStatus = document.getElementById("uploadStatus");
+  var fileInput = document.getElementById("fileInput");
+  var droppedFiles;
+
+  function overrideDefault(event) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  function fileHover() {
+    dropFileForm.classList.add("fileHover");
+  }
+
+  function fileHoverEnd() {
+    dropFileForm.classList.remove("fileHover");
+  }
+
+  function addFiles(event) {
+    droppedFiles = event.target.files || event.dataTransfer.files;
+    showFiles(droppedFiles);
+  }
+
+  function showFiles(files) {
+    if (files.length > 1) {
+      fileLabelText.innerText = files.length + " files selected";
+    } else {
+      fileLabelText.innerText = files[0].name;
+    }
+  }
+
+  function uploadFiles(event) {
+    event.preventDefault();
+    changeStatus("Uploading...");
+
+    var formData = new FormData();
+
+    for (var i = 0, file; (file = droppedFiles[i]); i++) {
+      formData.append(fileInput.name, file, file.name);
+    }
+
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function (data) {
+      //handle server response and change status of
+      //upload process via changeStatus(text)
+      console.log(xhr.response);
+    };
+    xhr.open(dropFileForm.method, dropFileForm.action, true);
+    xhr.send(formData);
+  }
+
+  function changeStatus(text) {
+    uploadStatus.innerText = text;
+  }
---
+++ Code (Django Project)/zipFile/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ZipfileConfig(AppConfig):
+    name = 'zipFile'
